c5db1f021ffb417521e2c9ee2518ac08799cad01
Evan Nemerson <evan@nemerson.com>
2021-06-13 18:24:56 -0400

    Finish splat.

---
+++ lowering/i16x8.splat.c
@@ -0,0 +1,6 @@
+#include <simde/wasm/simd128.h>
+
+v128_t
+i16x8_splat(int16_t x) {
+  return wasm_i16x8_splat(x);
+}
---
+++ lowering/i64x2.splat.c
@@ -0,0 +1,6 @@
+#include <simde/wasm/simd128.h>
+
+v128_t
+i64x2_splat(int64_t x) {
+  return wasm_i64x2_splat(x);
+}
