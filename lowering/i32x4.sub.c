828db136057854cb6b056a3682637bda9ffcf860
Evan Nemerson <evan@nemerson.com>
2021-06-19 14:17:51 -0400

    Add sub instructions.

---
+++ lowering/i32x4.sub.c
@@ -0,0 +1,6 @@
+#include <simde/wasm/simd128.h>
+
+v128_t
+i32x4_sub(v128_t a, v128_t b) {
+  return wasm_i32x4_sub(a, b);
+}
---
+++ lowering/i64x2.sub.c
@@ -0,0 +1,6 @@
+#include <simde/wasm/simd128.h>
+
+v128_t
+i64x2_sub(v128_t a, v128_t b) {
+  return wasm_i64x2_sub(a, b);
+}
