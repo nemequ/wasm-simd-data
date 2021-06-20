f55882367d14cba5f9de8b69231fd788c1e60b2b
Evan Nemerson <evan@nemerson.com>
2021-06-20 10:39:15 -0400

    Add shl.

---
+++ lowering/i32x4.shl.c
@@ -0,0 +1,6 @@
+#include <simde/wasm/simd128.h>
+
+v128_t
+i32x4_shl(v128_t a, int32_t b) {
+  return wasm_i32x4_shl(a, b);
+}
---
+++ lowering/i64x2.shl.c
@@ -0,0 +1,6 @@
+#include <simde/wasm/simd128.h>
+
+v128_t
+i64x2_shl(v128_t a, int32_t b) {
+  return wasm_i64x2_shl(a, b);
+}
