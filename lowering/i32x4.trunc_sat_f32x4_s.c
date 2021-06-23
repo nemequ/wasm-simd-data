4583453a1c4463e70a4260fd1efa802dadd24f7a
Evan Nemerson <evan@nemerson.com>
2021-06-23 10:16:38 -0400

    Add trunc_sat.

---
+++ lowering/i32x4.trunc_sat_f32x4_s.c
@@ -0,0 +1,6 @@
+#include <simde/wasm/simd128.h>
+
+v128_t
+i32x4_trunc_sat_f32x4(v128_t a) {
+  return wasm_i32x4_trunc_sat_f32x4(a);
+}
---
+++ lowering/i32x4.trunc_sat_f32x4_u.c
@@ -0,0 +1,6 @@
+#include <simde/wasm/simd128.h>
+
+v128_t
+u32x4_trunc_sat_f32x4(v128_t a) {
+  return wasm_u32x4_trunc_sat_f32x4(a);
+}
