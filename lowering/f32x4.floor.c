278f07bee95de9e3dcdb084d941a32674cb6ccb5
Evan Nemerson <evan@nemerson.com>
2021-06-22 23:33:28 -0400

    Add floor.

---
+++ lowering/f32x4.floor.c
@@ -0,0 +1,6 @@
+#include <simde/wasm/simd128.h>
+
+v128_t
+f32x4_floor(v128_t a) {
+  return wasm_f32x4_floor(a);
+}
---
+++ lowering/f64x2.floor.c
@@ -0,0 +1,6 @@
+#include <simde/wasm/simd128.h>
+
+v128_t
+f64x2_floor(v128_t a) {
+  return wasm_f64x2_floor(a);
+}
