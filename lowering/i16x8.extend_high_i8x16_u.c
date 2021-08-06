f66de1ce7fddd3dc75ca8baa99ddab3e3df05d77
Evan Nemerson <evan@nemerson.com>
2021-08-05 23:41:22 -0400

    Fix some extend_high/low calls
    these were calling the wrong functions.

--- lowering/i16x8.extend_high_i8x16_u.c
+++ lowering/i16x8.extend_high_i8x16_u.c
@@ -1,6 +1,6 @@
 #include <simde/wasm/simd128.h>
 
 v128_t
-u16x8_extend_low_u8x16(v128_t a) {
-  return wasm_u16x8_extend_low_u8x16(a);
+u16x8_extend_high_u8x16(v128_t a) {
+  return wasm_u16x8_extend_high_u8x16(a);
 }
--- lowering/i16x8.extend_low_i8x16_s.c
+++ lowering/i16x8.extend_low_i8x16_s.c
@@ -1,6 +1,6 @@
 #include <simde/wasm/simd128.h>
 
 v128_t
-i16x8_extend_high_i8x16(v128_t a) {
-  return wasm_i16x8_extend_high_i8x16(a);
+i16x8_extend_low_i8x16(v128_t a) {
+  return wasm_i16x8_extend_low_i8x16(a);
 }
